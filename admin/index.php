3104fc0c706377d2040ec21b0a1a2f051dd839bb
Lee Garner <lee@leegarner.com>
2018-04-04 11:16:29 -0700

    E_ALL fix

--- admin/index.php
+++ admin/index.php
@@ -1181,6 +1181,7 @@ case 'import':
 
 case 'edit':
     $Ev = new Evlist\Event($_REQUEST['eid']);
+    $rp_id = isset($_POST['rp_id']) && !empty($_POST['rp_id']) ? $_POST['rp_id'] : '';
     $content .= $Ev->Edit('', $rp_id, 'save'.$actionval);
     break;
 

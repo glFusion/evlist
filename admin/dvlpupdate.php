308913ed5e8d47ed10a2ff9f6c5db5b0254dc3c6
Lee Garner <lee@leegarner.com>
2018-04-09 13:25:55 -0700

    Add development update script

---
+++ admin/dvlpupdate.php
@@ -0,0 +1,52 @@
+<?php
+/**
+*   Apply updates to Evlist during development.
+*   Calls evlist_upgrade() with "ignore_errors" set so repeated SQL statements
+*   won't cause functions to abort.
+*
+*   Only updates from the previous released version.
+*
+*   @author     Mark R. Evans mark AT glfusion DOT org
+*   @copyright  Copyright (c) 2018 Lee Garner <lee@leegarner.com>
+*   @package    evlist
+*   @version    1.4.5
+*   @since      1.4.5
+*   @license    http://opensource.org/licenses/gpl-2.0.php
+*               GNU Public License v2 or later
+*   @filesource
+*/
+
+require_once '../../../lib-common.php';
+if (!SEC_inGroup('Root')) {
+    // Someone is trying to illegally access this page
+    COM_errorLog("Someone has tried to access the Evlist Development Code Upgrade Routine without proper permissions.  User id: {$_USER['uid']}, Username: {$_USER['username']}, IP: " . $_SERVER['REMOTE_ADDR'],1);
+    $display  = COM_siteHeader();
+    $display .= COM_startBlock($LANG27[12]);
+    $display .= $LANG27[12];
+    $display .= COM_endBlock();
+    $display .= COM_siteFooter(true);
+    echo $display;
+    exit;
+}
+require_once EVLIST_PI_PATH . '/upgrade.php';   // needed for set_version()
+CACHE_clear();
+Evlist\Cache::clear();
+
+function EVLIST_dvlp_145()
+{
+    global $_EV_CONF;
+
+    $_EV_CONF['pi_version'] = '1.4.4';
+    EVLIST_do_set_version('1.4.4');
+    plugin_upgrade_evlist(true);
+}
+
+// Call the function for the currently-installed codebase
+EVLIST_dvlp_145();
+
+// need to clear the template cache so do it here
+CACHE_clear();
+header('Location: '.$_CONF['site_admin_url'].'/plugins.php?msg=600');
+exit;
+
+?>

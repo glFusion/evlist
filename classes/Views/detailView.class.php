7b3080da6ce225f105b03452f096607548bfd810
Lee Garner <lee@leegarner.com>
2021-02-05 15:47:34 -0800

    No header shown for detail view

--- classes/Views/detailView.class.php
+++ classes/Views/detailView.class.php
@@ -4,9 +4,9 @@
  * Creates daily, weekly, monthly and yearly calendar views
  *
  * @author      Lee P. Garner <lee@leegarner.com>
- * @copyright   Copyright (c) 2017 Lee Garner <lee@leegarner.com
+ * @copyright   Copyright (c) 2017-2021 Lee Garner <lee@leegarner.com
  * @package     evlist
- * @version     v1.4.3
+ * @version     v1.5.0
  * @license     http://opensource.org/licenses/gpl-2.0.php
  *              GNU Public License v2 or later
  * @filesource
@@ -36,6 +36,17 @@ class detailView extends \Evlist\View
         $this->inc_dt_sel = false;  // disable date/range selections
         parent::__construct($year, $month, $day, $cat, $cal, $opts);
     }
-}
 
-?>
+
+    /**
+     * Display the common header for all calendar views.
+     * The event detail page does not use the header.
+     *
+     * @param   boolean $add_link   True to include a "Add Event" button
+     * @return  string          HTML for calendar header
+     */
+    public function Header($add_link = true)
+    {
+        return '';
+    }
+}

e20759505b3a3218bcee4f89012a2ef5c1dc0738
Lee Garner <lee@leegarner.com>
2022-05-05 13:56:42 -0700

    Implement popup date switcher

--- public_html/js/datepicker.js
+++ public_html/js/datepicker.js
@@ -8,6 +8,10 @@ $(document).ready(function(){
 		var id = $(this).attr('id');
 		ev_datetimepicker_timepicker(id);
 	});
+	$('[data-ev-calswitcher]').each (function(i, obj) {
+		ev_calswitcher($(this).attr('id'));
+	});
+
 });
 function ev_datetimepicker_datepicker( selector ) {
 	var currentDT = $("#"+selector).val();
@@ -28,3 +32,22 @@ function ev_datetimepicker_timepicker( selector ) {
 		step: 15,
 	});
 }
+
+function ev_calswitcher(selector) {
+	var sel_id = '#' + selector;
+	$(sel_id).datetimepicker({
+		lazyInit: true,
+		value: $(sel_id + "_val").val(),
+		format:'Y-m-d',
+		timepicker: false,
+		altField: sel_id + '_alt',
+		altFormat: 'yy-mm-dd',
+		onSelectDate( dp,$input ) {
+			var month = parseInt($input.val().substr(5,2),10);
+			var year  = parseInt($input.val().substr(0,4),10);
+			var day = parseInt($input.val().substr(8,2),10);
+			window.location.href = glfusionSiteUrl + "/evlist/index.php?year=" + year + "&month=" + month + "&day=" + day;
+		},
+	});
+}
+
